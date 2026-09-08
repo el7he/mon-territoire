2ceefa61c4d80c3bab53ccf969daa00cdc05daf8
Louis-Théo <toabenuts@gmail.com>
2026-09-08 14:07:51 +0200

    FEATURE/Base test infrastructure and a basic test for the detail sheet.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+// vite.config.ts
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'; // ← 'vitest/config', pas 'vite'
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    globals: true,
+  },
+});
